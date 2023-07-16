d10fa7b4a2f1e74add90d3f24cc13c2c1a872681
Djwes1998 <jwengedaniel98@gmail.com>
2023-07-16 04:21:32 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,23 +6,23 @@
  *
  * Return: Always 0.
  */
-int main()
+int main(void)
 {
-    int i, j;
+  	 int i, j;
 
-    for (i = 0; i <= 8; i++)
-    {
-        for (j = i + 1; j <= 9; j++)
-	{
-            putchar(i + '0');
-            putchar(j + '0');
-            if (!(i == 8 && j == 9))
-	    {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
+    	for (i = 0; i <= 8; i++)
+        {
+        	for (j = i + 1; j <= 9; j++)
+		{
+           		 putchar(i + '0');
+            		 putchar(j + '0');
+            		 if (!(i == 8 && j == 9))
+	               {
+                	 putchar(',');
+            		 putchar(' ');
+            	       }
+        	}
+       }
 
     putchar('\n');
 
