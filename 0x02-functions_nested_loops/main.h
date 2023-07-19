cd999f33970401904ba0722671aa5f49fec70509
Djwes1998 <jwengedaniel98@gmail.com>
2023-07-19 13:42:06 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,7 +5,16 @@
 /*
  * Main prototype
  */
-int _putchar(char c)
-int print_alphabet(void)
-int print_alphabet_x10(void)	
+int _putchar(char c);
+int print_alphabet(void);
+int print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int _print_sign(int a);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table (void);
+int add(int,int);
+void print_to_98(int a);
 #endif	
