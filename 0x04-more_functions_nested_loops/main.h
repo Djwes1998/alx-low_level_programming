6b487d5b731ed1c193c626a11e115f7a69b6b27e
Djwes1998 <jwengedaniel98@gmail.com>
2023-07-20 14:08:48 -0700

    main.h

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,30 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+#include <std.h>
+
+/*
+ * Main prototype
+ */
+int _putchar(char c);
+int print_alphabet(void);
+int print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int _print_sign(int a);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table (void);
+int add(int,int);
+void print_to_98(int a);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+#endif	
